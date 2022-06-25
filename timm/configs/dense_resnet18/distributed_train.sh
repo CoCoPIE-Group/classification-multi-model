17a58081d1ddda35ba48b068972713365b939310
kaixu <kai.xu@cocopie.ai>
2022-06-24 19:27:07 -0700

    initial commit

--- timm/configs/dense_resnet18/distributed_train.sh
+++ timm/configs/dense_resnet18/distributed_train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NUM_PROC=${1:-4}
 shift
-python -m torch.distributed.launch --nproc_per_node=$NUM_PROC train_script_main.py /data/imagenet \
+python -m torch.distributed.launch --nproc_per_node=$NUM_PROC train_script_main.py \
   --model resnet18 -b 256 --sched cosine --lr 0.1 --epochs 125 --warmup-epochs 5 \
   --weight-decay 1e-4 --reprob 0.4 --remode pixel \
   --config ./configs/dense_resnet18/dense_resnet18.json
