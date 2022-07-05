0b153823c45b19aa7b853c6e539dbefc387c9667
kaixu <kai.xu@cocopie.ai>
2022-07-04 21:21:07 -0700

    initial commit

--- timm/configs/dense_resnet50/distributed_train.sh
+++ timm/configs/dense_resnet50/distributed_train.sh
@@ -2,4 +2,4 @@
 NUM_PROC=${1:-4}
 shift
 python -m torch.distributed.launch --nproc_per_node=$NUM_PROC train_script_main.py \
-  --config ./configs/dense_resnet18/dense_resnet18.json
+  --config ./configs/dense_resnet50/args_ai_template.json
