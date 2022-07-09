ffc7040fff19e5a49ea94701ff2591a3f5d8f953
kaixu <kai.xu@cocopie.ai>
2022-07-08 21:46:57 -0700

    initial commit

--- timm/configs/dense_effnetb4/distributed_train.sh
+++ timm/configs/dense_effnetb4/distributed_train.sh
@@ -2,4 +2,4 @@
 NUM_PROC=${1:-4}
 shift
 python -m torch.distributed.launch --nproc_per_node=$NUM_PROC train_script_main.py \
-  --config ./configs/dense_effnetb0/dense_effnetb0.json
+  --config ./configs/dense_effnetb4/args_ai_template.json
